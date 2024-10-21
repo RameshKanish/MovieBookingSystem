599c6d1d941c69586de2efbca0fddbd03218c334
Ramesh Rs <rameshrs@centizen.com>
2024-10-21 10:26:35 +0530

    Remove the port

--- src/main/java/com/example/MovieBooking/config/WebConfig.java
+++ src/main/java/com/example/MovieBooking/config/WebConfig.java
@@ -10,10 +10,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // Apply CORS to all endpoints
-                .allowedOrigins("https://moviebookingsystem.onrender.com") // Frontend URL
-                .allowedMethods("GET", "POST", "PUT", "DELETE") // Allowed HTTP methods
-                .allowedHeaders("*") // Allow all headers
-                .allowCredentials(true); // If your app uses cookies or authentication
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173", "https://movie-booking-b8kw.onrender.com") // Frontend URL
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
